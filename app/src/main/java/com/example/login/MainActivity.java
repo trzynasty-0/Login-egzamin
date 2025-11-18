9d8ad048f208ecc76ca1783e5aa5490e877cfe22
trzynasty-0 <goniwiechamaja123@gmail.com>
2025-11-18 08:29:09 +0100

    gotowa wersja podstawowa

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.login;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText emailInput, hasloInput1, hasloInput2;
+    private TextView emailView;
+    private Button zatwierdzButton, generujButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        emailInput = findViewById(R.id.email);
+        hasloInput1 = findViewById(R.id.haslo1);
+        hasloInput2 = findViewById(R.id.haslo2);
+        emailView = findViewById(R.id.emailView);
+        zatwierdzButton = findViewById(R.id.zatwierdz);
+
+        zatwierdzButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        EmailSprawdzenie(emailInput.getText().toString(), hasloInput1.getText().toString(), hasloInput2.getText().toString());
+                    }
+                }
+        );
+
+    }
+    private void EmailSprawdzenie(String email, String haslo1, String haslo2){
+        if(!email.contains("@")){
+            emailView.setText("Nieprawidłowy adres email");
+        }
+        else if(!haslo2.equals(haslo1)){
+            emailView.setText("Hasła się różnią");
+        }
+        else{
+            emailView.setText("Hello" + email);
+        }
     }
 }
